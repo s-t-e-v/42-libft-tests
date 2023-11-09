014465d5406555101e6f55f395a9a0b8ba21f399
Steven Bandaogo <sbandaog@student.42.fr>
2023-11-09 21:52:38 +0100

    refactor test ft_isprint

--- ft_isprint/tests.c
+++ ft_isprint/tests.c
@@ -6,58 +6,89 @@
 /*   By: sbandaog <sbandaog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:35:34 by sbandaog          #+#    #+#             */
-/*   Updated: 2023/11/09 18:22:28 by sbandaog         ###   ########.fr       */
+/*   Updated: 2023/11/09 21:50:25 by sbandaog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <ctype.h>
+#include "../../libft.h"
 #define FT "ft_isprint"
 #define FUNC(c) ft_isprint(c)
-
-int ft_isprint(int c);
+#define OG_FUNC(c) isprint(c)
 
 static int tests_run = 0;
 static int tests_failed = 0;
 
 /* Test assert function */
 
-static void	test_assert(int condition, const char* message)
+static void	test_assert(int condition, const char* message, const char* expected, const char *actual)
 {
 	if (!condition) {
 		tests_failed++;
 		printf("FAIL: %s\n", message);
+		printf("    Expected: %s\n", expected);
+		printf("    Actual: %s\n", actual);
 	}
 	tests_run++;
 }
 
 /* Tests functions */
-static void	test_valid() {
-	// ---- edge cases
-	test_assert(FUNC(' ') == 1, FT "(' ')");
-	test_assert(FUNC('~') == 1, FT "('~')");
-	// ---- normal cases
-	test_assert(FUNC('5') == 1, FT "('5')");
-	test_assert(FUNC('\\') == 1, FT "('\\')");
-	test_assert(FUNC('f') == 1, FT "('f')");
+static void	tests() {
+	int i;
+	int strs[] = {
+		// ---- valid edge cases
+		20,
+		' ',
+		'~',
+		// ---- valid normal cases
+		'5',
+		'\\',
+		'f',
+		// ---- invalid edge cases
+		31,
+		127,
+		// ---- invalid normal cases
+		128,
+		174,
+		200,
+		255,
+		3000,
+		'\n',
+		'\x0a',
+		'\x1d',
+		0
+	};
+	char expected[1000];
+	char actual[1000];
+	char message[1000]; // Make sure this is large enough to hold your message
+
+	// ---- general cases
+	i = 0;
+	while (1)
+	{
+
+		sprintf(expected, "%d", OG_FUNC(strs[i]));
+		sprintf(actual, "%d", FUNC(strs[i]));
+		sprintf(message, "%s(%d)", FT, strs[i]);
+		test_assert(!((FUNC(strs[i]) != 0) ^ (OG_FUNC(strs[i]) != 0)), message, expected, actual);
+		if(strs[i] == '\0')
+			break;
+		i++;
+	}
+}
+
+
+static void	tests_valid() {
 }
 
 static void	test_invalid() {
-	// ---- edge cases
-	test_assert(FUNC(31) == 0, FT "(31)");
-	test_assert(FUNC(127) == 0, FT "(127)");
-	// ---- general cases
-	test_assert(FUNC(0) == 0, FT "(0)");
-	test_assert(FUNC(3000) == 0, FT "(3000)");
-	test_assert(FUNC('\n') == 0, FT "('\\n')");
-	test_assert(FUNC('\x0a') == 0, FT "('\\x0a')");
-	test_assert(FUNC('\x1d') == 0, FT "('\\x1d')");
 }
 
 /* Run tests */
 static void	run_tests()
 {
-	test_valid();
-	test_invalid();
+	tests();
 }
 
 int	main()
