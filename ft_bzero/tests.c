125fd78a1690a0315b2772b4956599cc82451134
Steven Bandaogo <sbandaog@student.42.fr>
2023-11-10 18:03:54 +0100

    ft_bzero tests v1

--- ft_bzero/tests.c
+++ ft_bzero/tests.c
@@ -6,36 +6,20 @@
 /*   By: sbandaog <sbandaog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:35:34 by sbandaog          #+#    #+#             */
-/*   Updated: 2023/11/10 17:33:42 by sbandaog         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:53:28 by sbandaog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <bsd/string.h>
+#include <strings.h>
 #include "../../libft.h"
-#define FT "ft_memset"
-#define FUNC(a, b, c) ft_memset(a, b, c)
-#define OG_FUNC(a, b, c) memset(a, b, c)
+#define FT "ft_bzero"
+#define FUNC(a, b) ft_bzero(a, b)
+#define OG_FUNC(a, b) bzero(a, b)
 
 static int tests_run = 0;
 static int tests_failed = 0;
-static int c[] = {
-		0,
-		1,
-		10,
-		100,
-		127,
-		128,
-		150,
-		3000,
-		-1,
-		-10,
-		-100,
-		-127,
-		-128,
-		-150,
-		-3000
-	};
 
 typedef struct s_test_data {
 	char expected[1000];
@@ -57,62 +41,55 @@ static void	test_assert(int condition, t_test_data test)
 }
 
 /* Tests functions */
-static void	tests_char() {
-	size_t i;
-	size_t j;
-	size_t k;
-	t_test_data test;
-	char	data[] = {
-		'e',
-		'\n',
-		'\0',
-		'*',
-		'0',
-		'5',
-		127,
-		-1,
-		-10,
-		-50,
-		-128
-	};
-	
-	int n = sizeof(data[0]);
-	char *res;
-	char *res_og;
-	char data2[sizeof(data) / sizeof(data[0])];
-	char data_cpy[sizeof(data) / sizeof(data[0])];
-    char data2_cpy[sizeof(data2) / sizeof(data2[0])];
-	
-	// ---- general cases
-	i = 0;
-	while (i < sizeof(data) / sizeof(data[0]))
-	{
-		data2[i] = data[i];
-		data_cpy[i] = data[i];
-        data2_cpy[i] = data2[i];		
-		i++;
-	}
+static void tests_char() {
+    size_t i;
+    t_test_data test;
+    char data[] = {
+        'e',
+        '\n',
+        '\0',
+        '*',
+        '0',
+        '5',
+        127,
+        -1,
+        -10,
+        -50,
+        -128
+    };
 
-	i = 0;
-	while (i < sizeof(data) / sizeof(data[0]))
-	{
-		j = 0;
-		while (j < sizeof(c) / sizeof(c[0]))
-		{
-			sprintf(test.message, "%s(%d, %d, %d)", FT, data[i], c[j], n);
-			res = (char *)FUNC(&data[i], c[j], n);
-			res_og = (char *)OG_FUNC(&data2[i], c[j], n);
-			sprintf(test.actual, "%d (char)", *res);
-			sprintf(test.expected, "%d (char)", *res_og);
-			test_assert(memcmp(&res, &res_og, sizeof(res)), test);
-			// reinitialize current data and data2
-			memcpy(data, data_cpy, sizeof(data));
-			memcpy(data2, data2_cpy, sizeof(data2));
-				
-			j++;
-		}
-		i++;
-	}
+    size_t n = sizeof(data[0]);
+    char data2[sizeof(data) / sizeof(data[0])];
+    char data_cpy[sizeof(data) / sizeof(data[0])];
+
+    // ---- general cases
+    i = 0;
+    while (i < sizeof(data) / sizeof(data[0]))
+    {
+        data2[i] = data[i];
+        data_cpy[i] = data[i];
+        i++;
+    }
+
+    i = 0;
+    while (i < sizeof(data) / sizeof(data[0]))
+    {
+        sprintf(test.message, "%s(%d, %ld)", FT, data[i], n);
+
+        FUNC(&data[i], n);
+        OG_FUNC(&data2[i], n);
+
+        int result = memcmp(&data[i], &data2[i], sizeof(data[0]));
+        sprintf(test.actual, "%d", result);
+        sprintf(test.expected, "0");
+        test_assert(result == 0, test);
+
+        // reinitialize current data and data2
+        memcpy(data, data_cpy, sizeof(data));
+        memcpy(data2, data_cpy, sizeof(data2));
+
+        i++;
+    }
 }
 
 /* Run tests */
